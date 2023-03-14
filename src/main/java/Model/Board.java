a128491d409a09a6df17d42674da6cea6620eec0
AlessioSpineto <alessio.spineto@mail.polimi.it>
2023-03-14 15:09:41 +0100

    Fixed some errors in Board and Bag

--- src/main/java/Model/Board.java
+++ src/main/java/Model/Board.java
@@ -1,6 +1,14 @@
 package Model;
 
+import Model.CommonGoals.*;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
 
 
 public class Board {
@@ -20,7 +28,7 @@ public class Board {
             File boardConf = new File("board.conf");
             Scanner reader = new Scanner(boardConf);
 
-            for(int i = 0; i<9 && reader.hasNextLine; i++) {
+            for(int i = 0; i<9 && reader.hasNextLine(); i++) {
                 String data = reader.nextLine();
                 for (int j = 0; j<9; j++){
                     this.matrix[j][i] = new Cell(data[j]);
@@ -37,26 +45,24 @@ public class Board {
 
 //GOALS ARE MISSING
         setOfCommonGoal = new Set<CommonGoal>;
-        tempCG = null;
         Random rand = new Random()
         if(fm = true){
             rand.nextInt(10);
             switch rand.nextInt(10){
-                case 0: tempCG = setOfCommonGoal.insert(new GoalAngles);
+                case 0: setOfCommonGoal.add(new GoalAngles);
                     break;
             }
         }else{
             rand.nextInt(10);
             switch rand.nextInt(10){
-                case 0: tempCG = setOfCommonGoal.insert(new GoalAngles);
+                case 0: setOfCommonGoal.add(new GoalAngles);
                     break;
             }
             rand.nextInt(10);
             switch rand.nextInt(10){
-                case 0: tempCG = setOfCommonGoal.insert(new GoalAngles);
+                case 0: setOfCommonGoal.add(new GoalAngles);
                     break;
             }
-            setOfCommonGoal.insert(tempCG);
         }
 
         throw new NotImplementedException;
@@ -66,12 +72,12 @@ public class Board {
 
 
     public void endMatch(){
-        throw new NotImplementedException;
+        throw new NotImplementedException();
     }
     public void removeTile(Tile tile){
-        throw new NotImplementedException;
+        throw new NotImplementedException();
     }
     public void checkRecharge(){
-        throw new NotImplementedException;
+        throw new NotImplementedException();
     }
 }

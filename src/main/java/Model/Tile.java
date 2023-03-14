d004381b5f2ef1e7db36c57a4b729b1eb00d4dca
clara <clara.spezzi@mail.polimi.it>
2023-03-14 15:06:21 +0100

    tile fixed

--- src/main/java/Model/Tile.java
+++ src/main/java/Model/Tile.java
@@ -3,10 +3,10 @@ package Model;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Tile{
-
     private Color color;
 
-    public Tile Tile(Color c){
+    public void Tile(Color c){
+
         this.color = c;
         throw new NotImplementedException;
     }

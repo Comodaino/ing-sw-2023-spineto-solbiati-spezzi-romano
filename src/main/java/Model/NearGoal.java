fd652e80d3be0cf85599076c6798fa260a01777e
Alessio Spineto <alespineto@gmail.com>
2023-03-21 11:08:08 +0000

    Update to Controller

---
+++ src/main/java/Model/NearGoal.java
@@ -0,0 +1,47 @@
+package Model;
+
+public class NearGoal extends Goal{
+    private int score;
+    private boolean matrix[][];
+    public NearGoal(){
+        this.score=0;
+        this.matrix = new boolean[6][5];
+        for(int i=0; i<6; i++){
+            for(int j=0; j<5; j++){
+                this.matrix[i][j]=false;
+            }
+        }
+    }
+    public int getScore(Player p){
+        int counter=0;
+        for(int i=0; i<6; i++){
+            for(int j=0; j<6; j++) {
+                counter = 0;
+                matrix[i][j]=true;
+                counter=iterator(i,j,p);
+                switch (counter){
+                    case: ;
+                }
+            }
+        }
+        return score;
+    }
+    public int iterator(int i, int j, Player p){
+        int counter=0;
+            if(p.getShelf().getTile(i,j).getColor().equals(p.getShelf().getTile(i+1,j).getColor())){
+                counter+=1;
+                matrix[i][j]=true;
+                counter += iterator(i+1, j, p);
+            }
+            if(p.getShelf().getTile(i,j).getColor().equals(p.getShelf().getTile(i,j+1).getColor())){
+                counter+=1;
+                matrix[i][j]=true;
+                counter += iterator(i, j+1, p);
+            }
+            return counter;
+    }
+    @Override
+    public boolean isCompleted() {
+        return false;
+    }
+}

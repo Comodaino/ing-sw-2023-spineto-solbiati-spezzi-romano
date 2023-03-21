fbb3e52ea0d7bea5e7980497023d367f39d72740
nicosolbia <nicolo.solbiati@mail.polimi.it>
2023-03-21 14:30:29 +0100

    fixed some bugs (null safe) and shelf config

--- src/main/java/Model/CommonGoals/GoalSquares.java
+++ src/main/java/Model/CommonGoals/GoalSquares.java
@@ -17,12 +17,18 @@ public class GoalSquares extends CommonGoal{
         for(int i=0; i<5; i++){
             for(int j=0; j<4; j++){
                 if(((!foundMatrix[i][j] && !foundMatrix[i+1][j+1])&&(!foundMatrix[i+1][j] && !foundMatrix[i][j+1]))){
-                    if(((p.getShelf().getTile(i,j).getColor().equals(p.getShelf().getTile(i+1,j+1).getColor())) && (p.getShelf().getTile(i+1,j).getColor().equals(p.getShelf().getTile(i,j+1).getColor()))) &&  p.getShelf().getTile(i,j).getColor().equals(p.getShelf().getTile(i,j+1).getColor()) ) {
-                        counter++;
-                        foundMatrix[i][j] = true;
-                        foundMatrix[i + 1][j] = true;
-                        foundMatrix[i + 1][j + 1] = true;
-                        foundMatrix[i][j + 1] = true;
+                    if(p.getShelf().getTile(i,j)!=null && p.getShelf().getTile(i+1,j+1)!=null &&
+                            p.getShelf().getTile(i+1,j)!=null && p.getShelf().getTile(i,j+1)!=null){
+
+                        if(((p.getShelf().getTile(i,j).getColor().equals(p.getShelf().getTile(i+1,j+1).getColor())) &&
+                                (p.getShelf().getTile(i+1,j).getColor().equals(p.getShelf().getTile(i,j+1).getColor()))) &&
+                                p.getShelf().getTile(i,j).getColor().equals(p.getShelf().getTile(i,j+1).getColor())){
+                            counter++;
+                            foundMatrix[i][j] = true;
+                            foundMatrix[i + 1][j] = true;
+                            foundMatrix[i + 1][j + 1] = true;
+                            foundMatrix[i][j + 1] = true;
+                        }
                     }
                 }
             }

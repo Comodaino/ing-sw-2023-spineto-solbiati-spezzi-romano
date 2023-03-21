ed4602a4e2c3cf5eb1f0b6982575c0574e7260ca
Alessio Spineto <alespineto@gmail.com>
2023-03-21 15:00:32 +0000

    Updated NearGoal

--- src/main/java/Model/NearGoal.java
+++ src/main/java/Model/NearGoal.java
@@ -41,16 +41,20 @@ public class NearGoal extends Goal{
     }
     public int iterator(int i, int j, Player p){
         int counter=0;
-            if(p.getShelf().getTile(i,j).getColor().equals(p.getShelf().getTile(i+1,j).getColor())){
-                counter+=1;
-                matrix[i][j]=true;
-                counter += iterator(i+1, j, p);
+        if( i < 6 && p.getShelf().getTile(i+1,j)!=null) {
+            if (p.getShelf().getTile(i, j).getColor().equals(p.getShelf().getTile(i + 1, j).getColor())) {
+                counter += 1;
+                matrix[i][j] = true;
+                counter += iterator(i + 1, j, p);
             }
-            if(p.getShelf().getTile(i,j).getColor().equals(p.getShelf().getTile(i,j+1).getColor())){
-                counter+=1;
-                matrix[i][j]=true;
-                counter += iterator(i, j+1, p);
+        }
+        if( j < 5  && p.getShelf().getTile(i+1,j)!=null) {
+            if (p.getShelf().getTile(i, j).getColor().equals(p.getShelf().getTile(i, j + 1).getColor())) {
+                counter += 1;
+                matrix[i][j] = true;
+                counter += iterator(i, j + 1, p);
             }
+        }
             return counter;
     }
     @Override
--- src/test/java/Model/NearGoalTest.java
+++ src/test/java/Model/NearGoalTest.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NearGoalTest {
 
@@ -18,6 +18,7 @@ class NearGoalTest {
         Tile m[][] = new Tile[6][5];
 
         Scanner scanner = null;
+        String inputFile = null;
         try {
             File shelfFile = new File("src/test/java/Model/Test_confs/shelfTest1.json");
             scanner = new Scanner(shelfFile);
@@ -25,20 +26,32 @@ class NearGoalTest {
             throw new RuntimeException(e);
         }
         for(int row=0; row<6; row++){
-            String inputFile = scanner.nextLine();
+            inputFile = scanner.nextLine();
             for(int col=0; col<5; col++){
                 switch(inputFile.charAt(col)){
-                    case 'w': m[row][col] = new Tile(Color.WHITE);
+                    case 'W': m[row][col] = new Tile(Color.WHITE);
+                        break;
                     case 'Y': m[row][col] = new Tile(Color.YELLOW);
+                        break;
                     case 'P': m[row][col] = new Tile(Color.PINK);
+                        break;
                     case 'B': m[row][col] = new Tile(Color.BLUE);
+                        break;
                     case 'L': m[row][col] = new Tile(Color.LIGHTBLUE);
+                        break;
                     case 'G': m[row][col] = new Tile(Color.GREEN);
                     break;
                 }
 
             }
         }
+        for(int i=0; i<6; i++){
+            for(int j=0; j<5; j++){
+                System.out.print(j + m[i][j].getColor().toString());
+            }
+            System.out.println(i);
+        }
+
         this.player = new Player("player1", true);
         player.setShelf(m);
         this.neargoal = new NearGoal();

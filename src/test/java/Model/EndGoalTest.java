6d638b0108c1f18f40da2f77151b4aa4bcccc43b
Aleromano01 <alessandra2.romano@mail.polimi.it>
2023-03-23 19:33:21 +0100

    update EndGoalTest

--- src/test/java/Model/EndGoalTest.java
+++ src/test/java/Model/EndGoalTest.java
@@ -3,11 +3,15 @@ package Model;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EndGoalTest {
     @Test
-    void GetSCore(){
+    void TestGetSCore(){
         Player player = new Player("Ale",false);
         EndGoal endGoal = new EndGoal();
         endGoal.getStatus();
@@ -19,6 +23,14 @@ class EndGoalTest {
         player.getShelf().addTile(1,tile2);
         player.getShelf().addTile(0,tile3);
         Assertions.assertEquals(0,endGoal.getScore(player));
+    }
+    @Test
+    void TestGetScoreIs1() throws FileNotFoundException {
+        Player player = new Player("Ale",false);
+        EndGoal endGoal = new EndGoal();
+        Shelf shelf = new Shelf();
+
+
     }
 
 

47cd9b6a170226f753e5e53ab7c8def27953e421
Aleromano01 <alessandra2.romano@mail.polimi.it>
2023-03-23 16:34:10 +0100

    update PersonalGoalTest

--- src/main/java/Model/PersonalGoal.java
+++ src/main/java/Model/PersonalGoal.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
 
 public class PersonalGoal extends Goal{
     private Shelf playerShelf;

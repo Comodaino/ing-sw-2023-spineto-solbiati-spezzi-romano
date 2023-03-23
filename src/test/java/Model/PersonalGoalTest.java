196d0f77964d2e41f618673e94fc6c75a507f030
Aleromano01 <alessandra2.romano@mail.polimi.it>
2023-03-23 15:48:53 +0100

    update PersonalGoal

--- src/test/java/Model/PersonalGoalTest.java
+++ src/test/java/Model/PersonalGoalTest.java
@@ -10,35 +10,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonalGoalTest {
 
     @Test
-    public void TestGetScoreNOMatchingTile(){
-        Shelf playerShelf = new Shelf();
-        PersonalGoal personalGoal= new PersonalGoal(playerShelf);
-        personalGoal.getPersGoal(0);
-        int expectedScore=0;
-        int actualScore=personalGoal.getScore(playerShelf);
-        Assertions.assertEquals(expectedScore,actualScore);
+    public void TestGetScoreNoMatchingTile(){
+        Shelf shelf = new Shelf();
+        ArrayList<Pgtype> pGoal = new ArrayList<>();
+        PersonalGoal personalGoal = new PersonalGoal(shelf);
+        personalGoal.getPGoal();
+        int i=personalGoal.getScore(shelf);
+        Assertions.assertEquals(0,i);
     }
     @Test
     public void TestGetScoreOneMatchingTile(){
-        Shelf playerShelf = new Shelf();
-        PersonalGoal personalGoal= new PersonalGoal(playerShelf);
-        Tile t= new Tile(Color.GREEN);
-        playerShelf.addTile(1,t);
-        personalGoal.getPersGoal(0);
-        Assertions.assertEquals(0,personalGoal.getScore(playerShelf));
-
     }
     @Test
     public void TestCreatePersGoal(){
         Shelf playerShelf = new Shelf();
         ArrayList<Pgtype> pGoal = new ArrayList<>();
         PersonalGoal personalGoal= new PersonalGoal(playerShelf);
-        personalGoal.getPersGoal(0);
         for(int i=0; i<personalGoal.getPGoal().size();i++){
             System.out.print(personalGoal.getPGoal().get(i).getColor().toString()+" ");
             System.out.print(personalGoal.getPGoal().get(i).getLine()+" ");
             System.out.println(personalGoal.getPGoal().get(i).getCol()+" ");
 
         }
+    }
+    @Test
+    public void testGetPersGoal() {
+        Shelf shelf = new Shelf();
+        PersonalGoal personalGoal = new PersonalGoal(shelf);
+        ArrayList<Pgtype> pGoal =new ArrayList<>();
+        Assertions.assertEquals(6, personalGoal.getPGoal().size());
     }
 }

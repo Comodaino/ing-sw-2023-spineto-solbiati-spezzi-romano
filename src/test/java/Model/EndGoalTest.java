65f4f1a56a1e2fc407d1f01c482ae9e12b20ae81
Aleromano01 <alessandra2.romano@mail.polimi.it>
2023-03-23 18:59:18 +0100

    update EndGoalTest

--- src/test/java/Model/EndGoalTest.java
+++ src/test/java/Model/EndGoalTest.java
@@ -1,13 +1,25 @@
 package Model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EndGoalTest {
     @Test
-    void TestisCompleted(){
-
+    void GetSCore(){
+        Player player = new Player("Ale",false);
+        EndGoal endGoal = new EndGoal();
+        endGoal.getStatus();
+        Shelf playerShelf = new Shelf();
+        Tile tile1 = new Tile(Color.LIGHTBLUE);
+        Tile tile2 = new Tile(Color.GREEN);
+        Tile tile3 = new Tile(Color.YELLOW);
+        player.getShelf().addTile(0,tile1);
+        player.getShelf().addTile(1,tile2);
+        player.getShelf().addTile(0,tile3);
+        Assertions.assertEquals(0,endGoal.getScore(player));
     }
+
 
 }

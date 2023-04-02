02eb4e04b6c79158f77ecd626fbc315000f3669c
nicosolbia <nicolo.solbiati@mail.polimi.it>
2023-04-02 16:22:00 +0200

    fixed couples and quartets according to new rules

--- src/main/java/Model/CommonGoals/GoalQuartets.java
+++ src/main/java/Model/CommonGoals/GoalQuartets.java
@@ -3,18 +3,18 @@ package Model.CommonGoals;
 import Model.Player;
 
 public class GoalQuartets extends CommonGoal{
+    private boolean[][] foundMatrix = new boolean[6][5];
     public GoalQuartets(int numOfPlayer){
         super(numOfPlayer);
+        for(int i=0; i<6; i++){
+            for(int j=0; j<5; j++){
+                this.foundMatrix[i][j] = false;
+            }
+        }
     }
     @Override
     public int getScore(Player p){
         int numOfQuartets = 0, n = 0;
-        boolean[][] foundMatrix = new boolean[6][5];
-        for(int i=0; i<6; i++){
-            for(int j=0; j<5; j++){
-                foundMatrix[i][j] = false;
-            }
-        }
 
         for(int r=0; r<6 && numOfQuartets<4; r++){
             n = 0;
@@ -28,9 +28,7 @@ public class GoalQuartets extends CommonGoal{
                 }
                 if(n==3){
                     numOfQuartets++;
-                    for(int i=c+1; i>=c-2; i--){
-                        foundMatrix[r][i] = true;
-                    }
+                    search(r, c-2, p);
                     break;
                 }
             }
@@ -48,17 +46,35 @@ public class GoalQuartets extends CommonGoal{
                 }
                 if(n==3){
                     numOfQuartets++;
-                    for(int i=r+1; i>=r-2; i--){
-                        foundMatrix[i][c] = true;
-                    }
+                    search(r-2, c, p);
                     break;
                 }
             }
         } //search "vertical quartets"
 
+        System.out.println(numOfQuartets);
         if(numOfQuartets==4 && !this.completed.contains(p)){
             return assignScore(p);
         }
         return 0;
+    }
+
+    public void search(int r, int c, Player p) {
+        foundMatrix[r][c]=true;
+        if(r+1<6){
+            if(p.getShelf().getTile(r + 1, c) != null && !foundMatrix[r+1][c]){
+                if(p.getShelf().getTile(r, c).getColor().equals(p.getShelf().getTile(r + 1, c).getColor())){
+                    search(r + 1, c, p);
+                }
+            }
+        }
+
+        if(c+1<5){
+            if(p.getShelf().getTile(r, c + 1) != null && !foundMatrix[r][c+1]) {
+                if(p.getShelf().getTile(r, c).getColor().equals(p.getShelf().getTile(r, c + 1).getColor())){
+                    search(r, c + 1, p);
+                }
+            }
+        }
     }
 }

28c8202a15ecf001d81a1c193322c29ec9173fda
Alessio Spineto <alespineto@gmail.com>
2023-04-04 14:17:38 +0200

    Deleted old controller

--- src/test/java/Controller/GameControllerTest.java
+++ src/test/java/Controller/GameControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
@@ -14,25 +13,37 @@ import java.util.Scanner;
 
 class GameControllerTest extends Observable{
     @Test
-    public void inputTest() throws FileNotFoundException {
+    public void inputTestTwo() throws FileNotFoundException {
         List<Player> playerList=new ArrayList<Player>();
         playerList.add(new Player("player1", true));
         playerList.add(new Player("player2", false));
         GameController testController = new GameController(playerList);
         Board gameBoard = testController.getBoard();
         this.addObserver(testController);
-        File commands = new File("src/test/java/Controller/controllerTestConf");
+        File commands = new File("src/test/java/Controller/controllerTestConf2");
         Scanner commandScanner = new Scanner(commands);
         while(commandScanner.hasNextLine()){
             setChanged();
+            System.out.println("current player: " + testController.getCurrentPlayer().getNickname());
             notifyObservers(commandScanner.nextLine());
-            for(int i=5; i>=0; i--){
-                for(int j=0; j<5; j++){
-                    if(gameBoard.getListOfPlayer().get(0).getShelf().getTile(i,j)!=null) System.out.print(gameBoard.getListOfPlayer().get(0).getShelf().getTile(i,j).getColor() + " ");
-                    else System.out.print("null ");
-                }
-                System.out.println();
-            }
+            System.out.println();
+        }
+    }
+    @Test
+    public void inputTestThree() throws FileNotFoundException {
+        List<Player> playerList=new ArrayList<Player>();
+        playerList.add(new Player("player1", false));
+        playerList.add(new Player("player2", true));
+        playerList.add(new Player("player3", false));
+        GameController testController = new GameController(playerList);
+        Board gameBoard = testController.getBoard();
+        this.addObserver(testController);
+        File commands = new File("src/test/java/Controller/controllerTestConf2");
+        Scanner commandScanner = new Scanner(commands);
+        while(commandScanner.hasNextLine()){
+            setChanged();
+            System.out.println("current player: " + testController.getCurrentPlayer().getNickname());
+            notifyObservers(commandScanner.nextLine());
             System.out.println();
         }
     }

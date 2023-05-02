a94c96d19ab31fdebefee49389902c666fe021fa
nicosolbia <nicolo.solbiati@mail.polimi.it>
2023-05-02 12:53:55 +0200

    added addPlayer and serialVersionUID

--- src/main/java/Model/BoardView.java
+++ src/main/java/Model/BoardView.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 public class BoardView implements Serializable {
+    static final long serialVersionUID = 1L;
     private final Board board;
     public BoardView(Board b){ this.board=b; }
     public Tile getTile(int r, int c){

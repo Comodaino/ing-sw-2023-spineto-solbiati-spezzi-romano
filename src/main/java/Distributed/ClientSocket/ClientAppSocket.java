5de2d96167b391aeb1aa9142b7147346c843f25e
Alessio Spineto <alespineto@gmail.com>
2023-05-07 13:07:51 +0200

    Added de-serialization of boardView

--- src/main/java/Distributed/ClientSocket/ClientAppSocket.java
+++ src/main/java/Distributed/ClientSocket/ClientAppSocket.java
@@ -1,12 +1,9 @@
 package Distributed.ClientSocket;
 
 import Distributed.Lobby;
-import Distributed.States;
+import Model.BoardView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -14,32 +11,42 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ClientAppSocket {
+    private final int port;
     private List<Lobby> lobbyList;
     private Socket socket;
     private Scanner in;
     private PrintWriter out;
-    private int port;
+    private BoardView boardView;
+    ObjectInputStream objIn;
 
-    public ClientAppSocket(int port){
+    public ClientAppSocket(int port) {
         this.port = port;
     }
+
+    public static void main(String[] args) throws IOException {
+        ClientAppSocket client = new ClientAppSocket(25565);
+        client.Connect();
+    }
+
     public void Connect() throws IOException {
         try {
+            objIn = new ObjectInputStream(socket.getInputStream());
             lobbyList = new ArrayList<Lobby>();
             lobbyList.add(new Lobby());
 
-            socket = new Socket(lobbyList.toString(),port);
+            socket = new Socket(lobbyList.toString(), port);
 
             in = new Scanner(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
             Scanner stdin = new Scanner(System.in);
 
-            String messageFromServer = in.nextLine();
-            System.out.println("Message from server "+messageFromServer);
 
-            try{
+
+            try {
                 boolean spin = true;
-                while(spin){
+                while (spin) {
+                    String messageFromServer = in.nextLine();
+                    System.out.println("Message from server " + messageFromServer);
                     switch (messageFromServer) {
                         case "/init":
                             System.out.println(in.nextLine());
@@ -48,38 +55,38 @@ public class ClientAppSocket {
                             out.flush();
                             break;
                         case "/wait":
-                            if (in.hasNextLine())
-                                System.out.println(in.nextLine());
                             break;
                         case "/play":
                             System.out.println(in.nextLine());
                             out.println(stdin.nextLine());
                             out.flush();
+                            break;
                         case "/end":
                             System.out.println("Game over");
+                            break;
                         case "/close":
+                            spin = false;
                             in.close();
                             out.close();
                             socket.close();
+                            break;
+                        case "/update":
+
+                            boardView = (BoardView) objIn.readObject();
+                            //TODO IMPLEMENT update();
+                            break;
                     }
 
 
-
-
-                    }
                 }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-
-            socket.close();
-        } catch (UnknownHostException e){
+        } catch (UnknownHostException e) {
             System.err.println("Error connecting to server");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-    public static void main(String[] args) throws IOException {
-        ClientAppSocket client = new ClientAppSocket(25565);
-        client.Connect();
     }
 
 

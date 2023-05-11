84ea0c648bcba908167e72645375309c5f943e8d
Aleromano01 <alessandra2.romano@mail.polimi.it>
2023-05-12 01:24:46 +0200

    update GUIclass

--- src/main/java/View/GUIclass.java
+++ src/main/java/View/GUIclass.java
@@ -1,5 +1,6 @@
 package View;
 
+import Distributed.AbstractClient;
 import Distributed.RemotePlayer;
 import Model.Board;
 import Model.Tile;
@@ -13,53 +14,76 @@ public class GUIclass extends JFrame{
     private RemotePlayer player;
 
     private State state;
+    private boolean chair;
 
     public static void Play() {
-
 
         JFrame play = new JFrame("Play");
         play.setLocationRelativeTo(null);
         play.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
-        JPanel board = new JPanel();
-        JPanel shelf = new JPanel();
 
-        Border border1 = BorderFactory.createLineBorder(Color.BLACK);
-        TitledBorder titledBorder1 = BorderFactory.createTitledBorder(border1, "The Board");
-        board.setBorder(titledBorder1);
-
-        Border border2 = BorderFactory.createLineBorder(Color.BLACK);
-        TitledBorder titledBorder2 = BorderFactory.createTitledBorder(border2, "MyShelf");
-        shelf.setBorder(titledBorder2);
-
-        ImageIcon imageBoard = new ImageIcon("/Users/alessandraromano/Desktop/git_tutorial/ProvaMarket/foto/livingroom.png");
-        ImageIcon imageShelf = new ImageIcon("/Users/alessandraromano/Desktop/git_tutorial/ProvaMarket/foto/bookshelf.png");
-        imageBoard.setImage(imageBoard.getImage().getScaledInstance(500, 500,50));
-        imageShelf.setImage(imageShelf.getImage().getScaledInstance(300,300,30));
-
-        JLabel imageOfBorard = new JLabel(imageBoard);
-        JLabel imageOfShelf = new JLabel(imageShelf);
-
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,board,shelf);
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,createBoard(),createShelf());
         splitPane.setDividerLocation(0.5);
 
         panel.add(splitPane);
-        board.add(imageOfBorard);
-        shelf.add(imageOfShelf);
-
         play.add(panel);
         play.pack();
         play.setVisible(true);
 
     }
+    public static JPanel createBoard(){
+        JPanel board = new JPanel();
+
+        Border border1 = BorderFactory.createLineBorder(Color.BLACK);
+        TitledBorder titledBorder1 = BorderFactory.createTitledBorder(border1, "The Board");
+        board.setBorder(titledBorder1);
+
+        ImageIcon imageBoard = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/livingroom.png");
+        imageBoard.setImage(imageBoard.getImage().getScaledInstance(750, 750,50));
+
+        JLabel imageOfBoard = new JLabel(imageBoard);
+        board.add(imageOfBoard);
+
+
+        return board;
+    }
+    public static JPanel createShelf(AbstractClient client){
+        JPanel shelf = new JPanel();
+
+        Border border2 = BorderFactory.createLineBorder(Color.BLACK);
+        TitledBorder titledBorder2 = BorderFactory.createTitledBorder(border2, "MyShelf");
+        shelf.setBorder(titledBorder2);
+
+        ImageIcon imageShelf = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/bookshelf.png");
+        imageShelf.setImage(imageShelf.getImage().getScaledInstance(500,500,30));
+
+        JLabel imageOfShelf = new JLabel(imageShelf);
+
+        shelf.add(imageOfShelf);
+
+        if(client.getPlayer().getChair == true){
+            ImageIcon imageChair = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/firstplayertoken.png");
+            JLabel chairImage = new JLabel(imageChair);
+            shelf.add(chairImage);
+        }
+
+        return shelf;
+    }
     public static void Home(){
+        JFrame home = new JFrame();
+        home.setLayout(new BorderLayout());
+        // TODO aggiungere immagine logo a NORTH
+        // TODO aggiungere pulsante start a SOUTH
+        // TODO sistemare update nel caso in cui il  nickname è sbagliato
+
 
     }
     public static void Lobby(){
 
     }
 
-    public void GUIclass(AbstractClient client){
+    public void GUIclass(AbstractClient client){;
         this.player= client.getPlayer();
         switch (this.state){
             case HOME:{
@@ -77,6 +101,7 @@ public class GUIclass extends JFrame{
         }
     }
     public void update(String arg, Board board){
+
 
 
     }

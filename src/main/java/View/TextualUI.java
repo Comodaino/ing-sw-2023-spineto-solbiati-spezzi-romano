3cac31f4f469a564370fe72b3cfb6d3d62f8b2c8
clara <clara.spezzi@mail.polimi.it>
2023-05-11 19:43:59 +0200

    fixed some lines

--- src/main/java/View/TextualUI.java
+++ src/main/java/View/TextualUI.java
@@ -32,19 +32,25 @@ public class TextualUI extends ViewInterface {
     }
 
     public void update(String arg) {
+        String command;
         switch (this.state) {
             case HOME:
+                System.out.println("WELCOME TO MY SHELFIE !/s");
                 homePrint(arg);
                 break;
             case LOBBY:
                 if (player.isOwner()) {
-                    if (player.isOwner()) {
-                        System.out.println("write /play to start the game");
-                    } else
-                        System.out.println(player.getNickname() + "wait for the owner to start the game with command /play");
+                    System.out.println("Commands you can use:");
+                    System.out.println("/start to start the game");
+                    System.out.println("/firstMatch if this is your first match/sOR");
+                    System.out.println("/notFirstMatch if you have already played");
+                    do {
+                        command = input.nextLine();
+                        client.println(command);
+                    } while (!command.equals("/start"));
+                } else{
+                        System.out.println("wait for the owner to start the game");
                 }
-
-
                 break;
             case PLAY:
                 break;
@@ -52,11 +58,15 @@ public class TextualUI extends ViewInterface {
     }
 
     public void homePrint(String arg) {
-        System.out.println("WELCOME TO MY SHELFIE !/s");
         if (arg.equals(null)) {
             System.out.println("Please insert your nickname here: ");
             client.println(input.nextLine());
             //TODO inserire controllo nickname
         }
+        else{
+            System.out.println("this nickname is already used :( /s/splease insert another nickname ");
+            client.println(input.nextLine());
+        }
+
     }
 }

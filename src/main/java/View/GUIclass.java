9ff95b2271d0ded496cc7731ef694c4b786d3cef
Aleromano01 <alessandra2.romano@mail.polimi.it>
2023-05-13 20:45:46 +0200

    update GUIclass

--- src/main/java/View/GUIclass.java
+++ src/main/java/View/GUIclass.java
@@ -3,12 +3,14 @@ package View;
 import Distributed.AbstractClient;
 import Distributed.RemotePlayer;
 import Model.Board;
+import Model.Player;
 import Model.Tile;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.util.Random;
 
 public class GUIclass extends JFrame{
     private RemotePlayer player;
@@ -44,22 +46,22 @@ public class GUIclass extends JFrame{
         JLabel imageOfBoard = new JLabel(imageBoard);
         board.add(imageOfBoard);
 
-       /*    DA CONTROLLARE (NON MOSTRA LA TILE CAT1)
+       //    DA CONTROLLARE (NON MOSTRA LA TILE CAT1)
         JPanel tilePanel = new JPanel();
 
         tilePanel.setOpaque(false);
-        tilePanel.setLayout(null);
+  //      tilePanel.setLayout(null);
 
         ImageIcon  imageCat1 = new ImageIcon("/Users/alessandraromano/Desktop/git_tutorial/ProvaMarket/foto/Tile/Gatti1.1.png");
-        imageCat1.setImage(imageCat1.getImage().getScaledInstance(50,50,50));
+      //  imageCat1.setImage(imageCat1.getImage().getScaledInstance(50,50,50));
         JLabel tileCat1 = new JLabel(imageCat1);
         tileCat1.setBounds(10,10,imageCat1.getIconWidth(),imageCat1.getIconHeight());
         tilePanel.add(tileCat1);
 
 
-        tilePanel.setBounds(0, 0, imageBoard.getIconWidth(), imageBoard.getIconHeight());
+    //    tilePanel.setBounds(0, 0, imageBoard.getIconWidth(), imageBoard.getIconHeight());
         board.add(tilePanel);
-*/
+
         return board;
     }
     public static JPanel createShelf(AbstractClient client){
@@ -77,14 +79,65 @@ public class GUIclass extends JFrame{
 
         shelf.add(imageOfShelf, BorderLayout.NORTH);
 
-        if(client.getPlayer().getChair == true){
+        if(client.getPlayer().getModelPlayer().getChair() == true){
             ImageIcon imageChair = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/firstplayertoken.png");
             JLabel chairImage = new JLabel(imageChair);
             shelf.add(chairImage,BorderLayout.WEST);
             imageChair.setImage(imageChair.getImage().getScaledInstance(100,100,100));
         }
+        ImageIcon imageIcon = new ImageIcon(createPersonalGoal(client).getImage());
+        JLabel personalGoal = new JLabel(imageIcon);
+
+        shelf.add(personalGoal,BorderLayout.EAST);
 
         return shelf;
+    }
+    public static ImageIcon createPersonalGoal(AbstractClient client){
+        RemotePlayer player1 = client.getPlayer();
+        int persGoal = player1.getModelPlayer().getGoal().CreatePersonalGoal();
+    //    ImageIcon imagePersGoal = new ImageIcon();
+        switch (persGoal){
+
+            case 0: ImageIcon imagePersGoal1 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals12.png");
+                    return imagePersGoal1;
+                break;
+            case 1: ImageIcon imagePersGoal2 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals9.png");
+                    return imagePersGoal2;
+                break;
+            case 2: ImageIcon imagePersGoal3 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals2.png");
+                    return imagePersGoal3;
+                break;
+            case 3: ImageIcon imagePersGoal4 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals3.png");
+                    return imagePersGoal4;
+                break;
+            case 4: ImageIcon imagePersGoal5 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals4.png");
+                    return imagePersGoal5;
+                break;
+            case 5: ImageIcon imagePersGoal6 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals11.png");
+                    return imagePersGoal6;
+                break;
+            case 6: ImageIcon imagePersGoal7 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals5.png");
+                    return imagePersGoal7;
+                break;
+            case 7: ImageIcon imagePersGoal8 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals10.png");
+                    return imagePersGoal8;
+                break;
+            case 8: ImageIcon imagePersGoal9 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals.png");
+                    return imagePersGoal9;
+                break;
+            case 9: ImageIcon imagePersGoal10 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals6.png");
+                    return imagePersGoal10;
+                break;
+            case 10:ImageIcon imagePersGoal11 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals8.png");
+                    return imagePersGoal11;
+                break;
+            case 11:ImageIcon imagePersGoal12 = new ImageIcon("/Users/alessandraromano/Desktop/ALE/Proj Soft Eng/resources/Personal Goal /Personal_Goals7.png");
+                    return imagePersGoal12;
+                break;
+        }
+        return null;
+        //imagePersGoal.setImage(imagePersoGoal.getImage().getScaledInstance(100,100,100));
+       // JLabel imageOfPersGoal = new JLabel();
     }
     public static void Home(){
         JFrame home = new JFrame();
@@ -100,6 +153,7 @@ public class GUIclass extends JFrame{
     }
 
     public void GUIclass(AbstractClient client){;
+
         this.player= client.getPlayer();
         switch (this.state){
             case HOME:{

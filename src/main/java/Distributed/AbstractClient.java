a57a880a99a523fb9f4d07ca7e13cc00aa3176b3
clara <clara.spezzi@mail.polimi.it>
2023-05-14 18:27:08 +0200

    added case State.PLAY in TUI

--- src/main/java/Distributed/AbstractClient.java
+++ src/main/java/Distributed/AbstractClient.java
@@ -1,9 +1,12 @@
 package Distributed;
 
+import Model.BoardView;
+
 import java.io.IOException;
 
 public interface AbstractClient {
     public void println(String arg) throws IOException;
 
     RemotePlayer getPlayer();
+    BoardView getBoardView();
 }
--- src/main/java/View/TextualUI.java
+++ src/main/java/View/TextualUI.java
@@ -2,6 +2,7 @@ package View;
 
 import Distributed.AbstractClient;
 import Distributed.RemotePlayer;
+import Model.Tile;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -33,7 +34,6 @@ public class TextualUI extends ViewInterface {
     }
 
     public void inputHandler() throws IOException {
-        System.out.println("write a command\n");
         String command = input.nextLine();
         client.println(command);
 
@@ -46,6 +46,7 @@ public class TextualUI extends ViewInterface {
             case HOME:
                 System.out.println("WELCOME TO MY SHELFIE !\n");
                 homePrint(arg);
+                this.state = State.LOBBY;
                 break;
             case LOBBY:
                 if (player.isOwner()) {
@@ -59,22 +60,88 @@ public class TextualUI extends ViewInterface {
                     } while (!command.equals("/start"));
                 } else{
                         System.out.println("wait for the owner to start the game");
+                    try {
+                        wait();   //not sure yet
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
+                this.state = State.PLAY;
                 break;
             case PLAY:
-
+                System.out.println("Your turn!");
+                showBoard();
+                showShelf();
+                System.out.println("Commands you can use:");
+                System.out.println("/add");
+                System.out.println("/remove");
+                inputHandler();
                 break;
+        }
+    }
+
+    private void showShelf() {
+        System.out.println("YOUR SHELF:");
+        for(int i=0; i<6; i++){
+            for(int j=0; j<5; j++){
+                Tile tile = player.getModelPlayer().getShelf().getTile(i, j);
+                if (tile.equals(null)){
+                    System.out.print("    ");
+                }else{
+                    System.out.print(" " + tile.getColor().name().charAt(0));
+                    switch (tile.getType()){
+                        case ONE:
+                            System.out.print("1 ");
+                            break;
+                        case TWO:
+                            System.out.print("2 ");
+                            break;
+                        case THREE:
+                            System.out.print("3 ");
+                            break;
+                    }
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+
+    private void showBoard() {
+        System.out.println("showing the BOARD...");
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                Tile tile = client.getBoardView().getCell(i,j).getTile();
+                if(tile.equals(null)){
+                    System.out.print("|    |");
+                }else{
+                    System.out.print("| " + tile.getColor().name().charAt(0));
+                    switch (tile.getType()){
+                        case ONE:
+                            System.out.print("1 |");
+                            break;
+                        case TWO:
+                            System.out.print("2 |");
+                            break;
+                        case THREE:
+                            System.out.print("3 |");
+                            break;
+                    }
+                }
+
+
+            }
+            System.out.print("\n");
         }
     }
 
     public void homePrint(String arg) throws IOException {
         if (arg.equals("/nickname")) {
-            System.out.println("nickname already used \nplease insert another nickname:  ");
-            client.println(input.nextLine());
+            System.out.println("nickname already used, please insert another nickname:  ");
+            inputHandler();
         }
         else{
-            System.out.println("insert here your nickname:  ");
-            client.println(input.nextLine());
+            System.out.println("insert your nickname:  ");
+            inputHandler();
         }
 
     }

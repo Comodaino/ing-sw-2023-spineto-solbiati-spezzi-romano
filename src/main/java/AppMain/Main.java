fb35e64415bb89eb1d30d711f0bf26d7015318e5
Alessio Spineto <alespineto@gmail.com>
2023-06-17 09:43:54 +0200

    modified Main to start with args

--- src/main/java/AppMain/Main.java
+++ src/main/java/AppMain/Main.java
@@ -10,38 +10,93 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
 
+        String type = null;
+        String ip = null;
+        String view = null;
+        String connection = null;
 
-        if(args.length==1){
-            clientInput(args[0]);
-        }else {
+        if (args.length != 0){
+
+
+            for(int i = 0; i< args.length; i+=2) {
+                System.out.println("itering args");
+                if (args[i].startsWith("-")) {
+                    switch (args[i]) {
+                        case "-t":
+                            type = args[i+1];
+                            break;
+                        case "-i":
+                            ip = args[i+1];
+                            break;
+                        case "-v":
+                            view = args[i+1];
+                            break;
+                        case "-c":
+                            connection = args[i+1];
+                            break;
+                    }
+                }
+            }
+
+        }
+        if (args.length == 0 || type == null) {
             System.out.println(">>insert \"server\" or \"client\"");
             Scanner scanner = new Scanner(System.in);
-
-
             switch (scanner.nextLine()) {
                 case "server":
                     ServerApp.execute();
                     break;
                 case "client":
-                    clientInput(null);
+                    clientInput(ip, connection, view);
+                    break;
+            }
+        }else{
+            switch (type) {
+                case "server":
+                    ServerApp.execute();
+                    break;
+                case "client":
+                    clientInput(ip, connection, view);
                     break;
             }
         }
     }
 
-    private static void clientInput(String arg) {
-        System.out.println(">>insert \"socket\" or \"RMI\"");
-        Scanner scanner = new Scanner(System.in);
-        switch(scanner.nextLine()){
-            case "socket":
-                try {
-                    ClientAppSocket.execute(arg);
-                } catch (IOException | ClassNotFoundException | InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                break;
-            case "RMI": ClientApp.execute();
-                break;
+
+
+
+
+    private static void clientInput(String ip, String connection, String view) {
+
+        if(connection == null){
+            System.out.println(">>insert \"socket\" or \"RMI\"");
+            Scanner scanner = new Scanner(System.in);
+            switch (scanner.nextLine()) {
+                case "socket":
+                    try {
+                        ClientAppSocket.execute(ip, view);
+                    } catch (IOException | ClassNotFoundException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case "RMI":
+                    ClientApp.execute();
+                    break;
+            }
+        }else{
+            switch (connection) {
+                case "socket":
+                    try {
+                        ClientAppSocket.execute(ip, view);
+                    } catch (IOException | ClassNotFoundException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case "RMI":
+                    ClientApp.execute();
+                    break;
+            }
         }
+
     }
 }

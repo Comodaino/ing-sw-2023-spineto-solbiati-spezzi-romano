8b9e70c830fd3da7d10255631d66d8e344f23337
Alessio <alessio.spineto@mail.polimi.it>
2023-06-18 11:32:46 +0200

    little change to controller

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -133,6 +133,7 @@ public class GameController implements Serializable {
         int first = input[1].charAt(0) - 48;
         int second = input[2].charAt(0) - 48;
         System.out.println("LENGTH: " + gameBoard.getTileBuffer().size());
+        if(gameBoard.getTileBuffer().size() > first || gameBoard.getTileBuffer().size() > second) return;
         Collections.swap(gameBoard.getTileBuffer(), first, second);
         System.out.println("swapped");
     }

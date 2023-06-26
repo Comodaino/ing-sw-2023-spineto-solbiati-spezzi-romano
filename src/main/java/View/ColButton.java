591974abbd699e7ee14534efae6831f348e16d99
Aleromano01 <alessandra2.romano@mail.polimi.it>
2023-06-26 12:33:41 +0200

    fixed ColButton

--- src/main/java/View/ColButton.java
+++ src/main/java/View/ColButton.java
@@ -4,6 +4,8 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 public class ColButton extends Button {
@@ -15,8 +17,8 @@ public ColButton(GUIApp parent, int index){
     this.index = index;
     this.parent = parent;
     button = new Button();
-    button.setStyle("-fx-background-color: green;");
 
+    button.setStyle("-fx-background-image: url('customFiles/columnButton.png') ;-fx-border-color: transparent; -fx-border-width: 0; -fx-background-repeat: no-repeat;");
     button.setPrefSize(45,20);
     button.setVisible(true);
     BooleanProperty isSelected = new SimpleBooleanProperty(false);
@@ -25,7 +27,6 @@ public ColButton(GUIApp parent, int index){
     this.button.setOnMouseClicked (e -> {
         if(isSelected.get()){
             isSelected.set(false);
-        //    button.setStyle("-fx-background-color: Red;");
             this.parent.setCommand("/add " + index);
             this.parent.setRemove(true);
         }

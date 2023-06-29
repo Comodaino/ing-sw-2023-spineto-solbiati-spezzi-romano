53b25a01005b13dc7f0a68a7f148655b72605d02
Alessio Spineto <alespineto@gmail.com>
2023-06-29 14:01:02 +0200

    unversioned

--- src/main/java/Distributed/ClientRMI/ClientApp.java
+++ src/main/java/Distributed/ClientRMI/ClientApp.java
@@ -17,7 +17,6 @@ import java.util.concurrent.TimeUnit;
 /**
  * ClientApp is a class which extends UnicastRemoteObject and implements Client and AbstractClient
  * It implements the RMI Client and all the methods through which it communicates with the RMI Server amd the ViewInterface
- *
  * @author Nicolò
  */
 public class ClientApp extends UnicastRemoteObject implements Client, AbstractClient {
@@ -31,7 +30,6 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
     private boolean owner;
     private double tmpStamp;
     private Timestamp timeStamp;
-    private boolean endFlag;
 
     public ClientApp(String typeOfView, String arg) throws RemoteException {
         nickname = null;
@@ -41,7 +39,7 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
         owner = false;
         state = States.INIT;
         timeStamp = new Timestamp(System.currentTimeMillis());
-        if (arg == null) address = "localhost";
+        if(arg == null) address = "localhost";
         else address = arg;
 
         if (typeOfView.equals("TUI")) {
@@ -51,7 +49,7 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
                 throw new RuntimeException(e);
             }
         }
-        if (typeOfView.equals("GUI")) {
+        if (typeOfView.equals("GUI")){
             PassParameters.setClient(this);
             PassParameters.setState(State.HOME);
             this.view = new GUIApp();
@@ -70,78 +68,66 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
     /**
      * This method is invoked by the view method inputHandler() to catch the input of a client from the keyboard.
      * It invokes the server handler method, passing the input of the client as a parameter.
-     *
      * @param arg the input written by the client (caught by the view)
      * @author Nicolò
      */
     @Override
     public void println(String arg) {
         try {
-            if (arg.equals("/exit") || arg.equals("/quit")) System.exit(0);
+            if(arg.equals("/exit")) System.exit(0);
 
             if (!state.equals(States.INIT) && !arg.startsWith("/")) arg = "/message " + nickname + " " + arg;
 
-            if (arg.startsWith("/whisper")) {
-                String[] tmp = arg.split(" ");
-                String last = tmp[tmp.length - 1];
+            if(arg.startsWith("/whisper")) {
+                String[] tmp =  arg.split(" ");
+                String last = tmp[tmp.length-1];
                 String[] tmp2 = new String[tmp.length];
                 tmp2[0] = tmp[0];
                 tmp2[1] = tmp[1];
-                for (int i = 1; i < tmp.length - 1; i++) {
-                    tmp2[i + 1] = tmp[i];
+                for(int i= 1 ; i < tmp.length - 1; i++ ){
+                    tmp2[i+1] = tmp[i];
                 }
                 tmp2[1] = this.nickname;
                 arg = tmp2[0];
-                for (int i = 1; i < tmp.length; i++) {
+                for(int i = 1; i< tmp.length; i++){
                     arg = arg + " " + tmp2[i];
                 }
                 arg = arg + " " + last;
-            } else {
-                if ((arg.startsWith("/remove") || arg.startsWith("/switch")) || arg.startsWith("/add")) {
-                    String[] tmp = arg.split(" ");
+            }else{
+                if((arg.startsWith("/remove") || arg.startsWith("/switch")) || arg.startsWith("/add")){
+                    String[] tmp =  arg.split(" ");
                     String newMsg = tmp[0] + " " + this.nickname + " ";
-                    for (int i = 1; i < tmp.length; i++) {
+                    for(int i = 1; i<tmp.length; i++){
                         newMsg += tmp[i] + " ";
                     }
-                    arg = newMsg;
+                    arg =newMsg;
                 }
             }
             server.handler(this, arg);
         } catch (IOException e) {
-            try {
-                view.update("disconnected");
-                return;
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
+            throw new RuntimeException(e);
         }
     }
 
 
+
     /**
      * This method is invoked by the server every time that a move is made. It invokes the view update and sets the boardView to the client.
-     *
      * @param boardView the serializable boardView
-     * @param arg       a message from the server (not always used)
+     * @param arg a message from the server (not always used)
      * @author Nicolò
      */
     @Override
     public void update(BoardView boardView, String arg) throws RemoteException {
 
         this.boardView = boardView;
-
-        if (this.state == States.END && endFlag) {
-            endFlag = false;
-            return;
-        }
-        endFlag = true;
-        if (arg == null || arg.length() == 0) {
+        if(arg==null || arg.length() == 0){
             try {
                 view.update();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        } else {
+        }else{
             try {
                 view.update(arg);
             } catch (IOException e) {
@@ -149,21 +135,19 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
             }
         }
 
-
     }
 
     /**
      * This method asks the Client which type of View does it want;
      * then creates an instance of ClientApp and connects it to the server through the method run().
-     *
      * @param typeOfView the type of View can be TUI (textual user interface) or GUI (graphical user interface)
-     * @param ip         the IP address the Client connects to
+     * @param ip the IP address the Client connects to
      * @author Nicolò
      */
     public static void execute(String typeOfView, String ip) {
 
         ClientApp client = null;
-        if (typeOfView == null) {
+        if(typeOfView == null){
             System.out.println(">>insert \"TUI\" or \"GUI\"");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
@@ -173,7 +157,7 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
             } catch (RemoteException e) {
                 throw new RuntimeException(e);
             }
-        } else {
+        }else{
             try {
                 client = new ClientApp(typeOfView, ip);
             } catch (RemoteException e) {
@@ -191,7 +175,6 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
 
     /**
      * This method take a reference of the Server from the RMI Registry and "connects" the Client to it.
-     *
      * @param serverHost the IP address or the name of the Server the Client connects to
      * @author Nicolò
      */
@@ -199,12 +182,12 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
         this.server = (Server) Naming.lookup("rmi://" + serverHost + "/ServerRMI");
 
 
-        Thread rmiHB = new Thread() {
+        Thread rmiHB = new Thread(){
             @Override
             public void run() {
-                while (true) {
+                while(true){
 
-                    if (heartBeatServiceClient()) {
+                    if(heartBeatServiceClient()){
                         timeStamp = new Timestamp(System.currentTimeMillis());
                     }
 
@@ -218,10 +201,10 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
             }
         };
 
-        Thread rmiHBCheck = new Thread() {
+        Thread rmiHBCheck = new Thread(){
             @Override
             public void run() {
-                while (true) {
+                while(true){
 
                     tmpStamp = timeStamp.getTime();
                     try {
@@ -229,7 +212,7 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
-                    if (tmpStamp == timeStamp.getTime()) {
+                    if(tmpStamp == timeStamp.getTime()){
                         try {
                             view.update("disconnected");
                         } catch (IOException e) {
@@ -258,41 +241,28 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
     /**
      * This method is invoked periodically by the Server to check if the Client is still connected.
      * It is used to implement the disconnection resilience.
-     *
      * @return true
      * @author Nico
      */
     @Override
-    public boolean beat() throws RemoteException {
-        return true;
-    }
+    public boolean beat() throws RemoteException { return true; }
 
 
     //SETTER AND GETTER METHODS
     @Override
-    public BoardView getBoardView() {
-        return this.boardView;
-    }
+    public BoardView getBoardView() { return this.boardView; }
 
     @Override
-    public void setNickname(String nickname) throws RemoteException {
-        this.nickname = nickname;
-    }
+    public void setNickname(String nickname) throws RemoteException { this.nickname = nickname; }
 
     @Override
-    public String getNickname() throws RemoteException {
-        return this.nickname;
-    }
+    public String getNickname() throws RemoteException { return this.nickname; }
 
     @Override
-    public void setLobbyID(Integer lobbyID) throws RemoteException {
-        this.lobbyID = lobbyID;
-    }
+    public void setLobbyID(Integer lobbyID) throws RemoteException { this.lobbyID = lobbyID; }
 
     @Override
-    public Integer getLobbyID() throws RemoteException {
-        return this.lobbyID;
-    }
+    public Integer getLobbyID() throws RemoteException { return this.lobbyID; }
 
     @Override
     public void setState(States state) throws RemoteException {
@@ -300,7 +270,6 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
         switch (state) { //It also sets the state of the view
             case PLAY:
                 view.setState(State.PLAY);
-                this.endFlag = false;
                 break;
             case WAIT:
                 view.setState(State.LOBBY);
@@ -315,17 +284,11 @@ public class ClientApp extends UnicastRemoteObject implements Client, AbstractCl
     }
 
     @Override
-    public States getState() throws RemoteException {
-        return this.state;
-    }
+    public States getState() throws RemoteException { return this.state; }
 
     @Override
-    public void setOwner(boolean owner) throws RemoteException {
-        this.owner = owner;
-    }
+    public void setOwner(boolean owner) throws RemoteException { this.owner = owner; }
 
     @Override
-    public boolean isOwner() throws RemoteException {
-        return this.owner;
-    }
+    public boolean isOwner() throws RemoteException { return this.owner; }
 }
